0779c400394dc766b8d1237d60c9b263d828b3ec
Unknown <Gregschulkey@gmail.com>
2018-08-05 16:03:01 -0400

    Feature: added model for incoming JSON

---
+++ app/src/main/java/theschulk/com/weatherdemo/Model/ForecastModel.java
@@ -0,0 +1,45 @@
+package theschulk.com.weatherdemo.Model;
+
+/**
+ * Model to hold the forecast for each day
+ */
+public class ForecastModel {
+
+    private String highTemp;
+    private String lowTemp;
+    private String forecastDate;
+    private String weatherIcon;
+
+
+    public String getHighTemp() {
+        return highTemp;
+    }
+
+    public void setHighTemp(String highTemp) {
+        this.highTemp = highTemp;
+    }
+
+    public String getLowTemp() {
+        return lowTemp;
+    }
+
+    public void setLowTemp(String lowTemp) {
+        this.lowTemp = lowTemp;
+    }
+
+    public String getForecastDate() {
+        return forecastDate;
+    }
+
+    public void setForecastDate(String forecastDate) {
+        this.forecastDate = forecastDate;
+    }
+
+    public String getWeatherIcon() {
+        return weatherIcon;
+    }
+
+    public void setWeatherIcon(String weatherIcon) {
+        this.weatherIcon = weatherIcon;
+    }
+}
